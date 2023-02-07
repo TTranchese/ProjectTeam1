8bff2afa1c19c48e808dd4652142dcb50e88f7ce
mircoForcella <forcella1996@gmail.com>
2023-02-07 12:12:23 +0100

    ADD CharacterAspect Classes
    ADD Class:
    BodyStyle
    FaceStyle
    HairStyle
    VoiceStyle
    ClothingStyle
    BodyStyle
    
    That combines CharacterAspect

---
+++ src/HairStyle.java
@@ -0,0 +1,35 @@
+public class HairStyle {
+    private String hairType;
+    private int lenght;
+    private String hairColor;
+
+    public HairStyle(String hairType, int lenght, String hairColor) {
+        this.hairType = hairType;
+        this.lenght = lenght;
+        this.hairColor = hairColor;
+    }
+
+    public String getHairType() {
+        return hairType;
+    }
+
+    public void setHairType(String hairType) {
+        this.hairType = hairType;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+}
