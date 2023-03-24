2c32ab5964b15f0ef7b1a8c34209cbb215f9564e
TTranchese <tommaso.tranchese.96@gmail.com>
2023-03-24 15:52:00 +0100

    added accountRepository class with test classes too

--- src/main/java/it/project1/account/Account.java
+++ src/main/java/it/project1/account/Account.java
@@ -3,7 +3,7 @@ package it.project1.account;
 
 import java.sql.*;
 
-public class Account implements IAccountRepository{
+public class Account{
 	private int id;
 	private String name;
 	private String password;
@@ -38,51 +38,4 @@ public class Account implements IAccountRepository{
 		this.id = id;
 	}
 
-
-
-
-	@Override
-	public boolean findAccount() {
-		Connection connection;
-		Statement stm = null;
-		ResultSet resultSet = null;
-			try {
-				connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mmo", "root", "root");
-				stm = connection.createStatement();
-				String query = "SELECT * FROM mmo.accounts WHERE nickName = '"+this.name+"' AND password = '"+this.password+"'";
-				resultSet = stm.executeQuery(query);
-
-				if ((resultSet.getInt(1)==this.id) && (resultSet.getString(2).equals(this.name))&&(resultSet.getString(3).equals(this.password))){
-					resultSet.close();
-					stm.close();
-					return true;
-				} else{
-					resultSet.close();
-					stm.close();
-					return false;
-				}
-			} catch (SQLException e) {
-				throw new RuntimeException(e);
-			}
-	}
-
-	@Override
-	public void insertAccount() {
-
-	}
-
-	@Override
-	public void deleteAccount() {
-
-	}
-
-	@Override
-	public void updateAccountName() {
-
-	}
-
-	@Override
-	public void updateAccountPassword() {
-
-	}
 }
--- src/main/java/it/project1/account/IAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package it.project1.account;
-
-public interface IAccountRepository {
-    public boolean findAccount();
-    public void insertAccount();
-    public void deleteAccount();
-    public void updateAccountName();
-    public void updateAccountPassword();
-}
