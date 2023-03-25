b0a262bc83df262aa0345336588e61ce4cc9d62c
TTranchese <tommaso.tranchese.96@gmail.com>
2023-03-25 16:42:37 +0100

    siplified account repo

---
+++ src/main/java/it/project1/Main.java
@@ -0,0 +1,9 @@
+package it.project1;
+
+import it.project1.account.AccountRepository;
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
--- src/main/java/it/project1/account/AccountRepository.java
+++ src/main/java/it/project1/account/AccountRepository.java
@@ -2,80 +2,75 @@ package it.project1.account;
 
 import java.sql.*;
 
-public class AccountRepository{
-    public AccountRepository(){};
-    public void createNewAccount(String nickName, String password){
-        Connection connection = null;
-        Statement statement = null;
-        String createAccountQuery = "INSERT INTO mmo.accounts (nickName, password) VALUES ('"+nickName+"', '"+password+"');";
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mmo", "root", "root");
-            statement=connection.createStatement();
-            statement.executeUpdate(createAccountQuery);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+public class AccountRepository {
+	private Connection connection;
+	private Statement statement;
+	private ResultSet resultSet;
 
-    public boolean findAccount(Account account) {
-        Connection connection;
-        Statement stm = null;
-        ResultSet resultSet = null;
-        boolean returnBool = false;
+	public AccountRepository() {
+		try {
+			this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mmo", "root", "root");
+			this.statement = connection.createStatement();
+			this.resultSet = null;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 
-        try{
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mmo", "root", "root");
-            stm = connection.createStatement();
-            String query = "SELECT * FROM mmo.accounts WHERE nickName = '" + account.getName() + "' AND password = '" + account.getPassword() + "'";
-            resultSet = stm.executeQuery(query);
-            while (resultSet.next()) {
-                if ((resultSet.getInt(1) == account.getId()) && (resultSet.getString(2).equals(account.getName())) && (resultSet.getString(3).equals(account.getPassword()))) {
-                    returnBool = true;
-                }
+	}
 
-            }
-        } catch (SQLException e){
-            throw new RuntimeException(e);
-        }
-        return returnBool;
-    }
+	;
 
-    public void deleteAccount(int id){
-        Connection connection = null;
-        Statement statement = null;
-        String deleteQuery = "DELETE FROM mmo.accounts WHERE id="+id;
-        try {
-             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mmo", "root", "root");
-             statement=connection.createStatement();
-             statement.executeUpdate(deleteQuery);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public void createNewAccount(String nickName, String password) {
+		//TODO aggiungere check se il nickname esiste già
+		String createAccountQuery = "INSERT INTO mmo.accounts (nickName, password) VALUES ('" + nickName + "', '" + password + "');";
+		try {
+			this.statement.executeUpdate(createAccountQuery);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    public void updatePassword(int id, String newPassword){
-        Connection connection = null;
-        Statement statement = null;
-        String updatePasswordQuery = "UPDATE mmo.accounts SET password = '"+newPassword+"' WHERE id = "+id;
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mmo", "root", "root");
-            statement=connection.createStatement();
-            statement.executeUpdate(updatePasswordQuery);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public boolean findAccount(Account account) {
+		boolean returnBool = false;
+		String query = "SELECT * FROM mmo.accounts WHERE nickName = '" + account.getName() + "' AND password = '" + account.getPassword() + "'";
+		try {
+			this.resultSet = this.statement.executeQuery(query);
+			while (resultSet.next()) {
+				if ((resultSet.getInt(1) == account.getId()) && (resultSet.getString(2).equals(account.getName())) && (resultSet.getString(3).equals(account.getPassword()))) {
+					returnBool = true;
+				}
 
-     public void updateUsername(int id, String nickName){
-         Connection connection = null;
-         Statement statement = null;
-         String updateNickNameQuery = "UPDATE mmo.accounts SET nickName = '"+nickName+"' WHERE id = "+id;
-         try {
-             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mmo", "root", "root");
-             statement=connection.createStatement();
-             statement.executeUpdate(updateNickNameQuery);
-         } catch (SQLException e) {
-             throw new RuntimeException(e);
-         }
-     }
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return returnBool;
+	}
+
+	public void deleteAccount(int id) {
+		String deleteQuery = "DELETE FROM mmo.accounts WHERE id=" + id;
+		try {
+			this.statement.executeUpdate(deleteQuery);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void updatePassword(int id, String newPassword) {
+		String updatePasswordQuery = "UPDATE mmo.accounts SET password = '" + newPassword + "' WHERE id = " + id;
+		try {
+			statement.executeUpdate(updatePasswordQuery);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void updateUsername(int id, String nickName) {
+		String updateNickNameQuery = "UPDATE mmo.accounts SET nickName = '" + nickName + "' WHERE id = " + id;
+		try {
+			statement.executeUpdate(updateNickNameQuery);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
