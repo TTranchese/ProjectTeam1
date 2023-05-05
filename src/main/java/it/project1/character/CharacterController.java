ec70952302c9b618cfe4ae2fe46268f666522c14
TTranchese <tommaso.tranchese.96@gmail.com>
2023-05-05 10:03:15 +0200

    added find all and find by id to characters

--- src/main/java/it/project1/character/CharacterController.java
+++ src/main/java/it/project1/character/CharacterController.java
@@ -1,6 +1,8 @@
 package it.project1.character;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,8 +17,13 @@ import java.util.Optional;
 public class CharacterController {
 	@Autowired
 	public CharacterService characterService;
-	@GetMapping("/findAll")
-	public Collection<CharacterEntity> getAll() {
+	@GetMapping
+	public ResponseEntity<CharacterEntity> getById(@RequestParam int id){
+		return characterService.getById(id);
+	}
+	@GetMapping("/find-all")
+	public ResponseEntity<List<CharacterEntity>> getAll() {
 		return characterService.getListOfCharacters();
 	}
+	
 }
--- src/main/java/it/project1/character/CharacterService.java
+++ src/main/java/it/project1/character/CharacterService.java
@@ -1,6 +1,9 @@
 package it.project1.character;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
@@ -8,7 +11,10 @@ public class CharacterService {
 	@Autowired
 	private CharacterRepository characterRepository;
 	
-	public List<CharacterEntity> getListOfCharacters() {
-		return characterRepository.findAll();
+	public ResponseEntity<List<CharacterEntity>> getListOfCharacters() {
+		return new ResponseEntity<>(characterRepository.findAll(), HttpStatus.OK);
+	}
+	public ResponseEntity<CharacterEntity> getById(int id){
+		return new ResponseEntity<>(characterRepository.findById(id).get(), HttpStatus.FOUND );
 	}
 }
