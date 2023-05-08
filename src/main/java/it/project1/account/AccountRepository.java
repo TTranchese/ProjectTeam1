f67f0a52055fa8367745d4da6173a69b900dae78
TTranchese <tommaso.tranchese.96@gmail.com>
2023-05-08 09:52:10 +0200

    changed monsterController and accountRepository

--- src/main/java/it/project1/account/AccountRepository.java
+++ src/main/java/it/project1/account/AccountRepository.java
@@ -2,6 +2,7 @@ package it.project1.account;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,5 @@ import java.util.List;
 @Repository
 public interface AccountRepository extends JpaRepository<AccountEntity, Integer> {
 	AccountEntity findById(int id);
-	
 	List<AccountEntity> findByNickName(String nickName);
 }
